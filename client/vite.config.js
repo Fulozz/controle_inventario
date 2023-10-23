084cb445ba3af335bdabe81f5a3276ea93ec472f
Fulozz <92411755+Fulozz@users.noreply.github.com>
2023-10-23 13:46:11 -0300

    git base update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'http://10.0.50.39',
   
-  server: {
-    host: 'http://10.0.50.39',
-  },
   
 })
