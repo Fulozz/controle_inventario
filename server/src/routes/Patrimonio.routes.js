39f1fe400297a081d120f6093be1995b6bd23380
Fulozz <92411755+Fulozz@users.noreply.github.com>
2023-10-26 09:32:01 -0300

    Patrimonio routes

---
+++ server/src/routes/Patrimonio.routes.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router();
+const productController = require('../controller/patrimonio.controller')
+
+
+
+
+
+
+// ==> Rota responsavel por criar os patrimonios: (GET) localhost:3000/api/v1/create
+router.post('/create',  productController.createPatrimonio)
+
+// ==> Rota responsavel por enviar para o frontend os patrimonios  e informações
+router.get('/listing',  productController.getPatrimonioListing)
+router.get('/all',  productController.allPatrimonio)
+router.get('/patrimonio/:id', productController.searchItem)
+
+// ==> Rota responsavel por atualizar os patrimonios: (GET) localhost:3000/api/v1/update/:id
+router.post('/update/:id',  productController.updatePatrimonio)
+
+// ==> Rota responsavel por atualizar os patrimonios: (GET) localhost:3000/api/v1/update/:id
+router.delete('/delete/:id',  productController.deletePatrimonio)
+
+// ==> Rota responsavel por procurar os patrimonios: (GET) localhost:3000/api/v1/search/:id
+router.post('/search/:id', productController.searchItem)
+
+router.get('/patrimonios', productController.countPatrimonios)
--- server/src/routes/User.routes.js
+++ server/src/routes/User.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/user.controller')
-const productController = require('../controller/patrimonio.controller')
+
 const auth  = require('../middleware/auth');
 
 // ==> Rota responsavel por criar o novo 'User': (POST) localhost:3000/api/v1/register
@@ -13,26 +13,6 @@ router.post('/login', userController.loginUser)
 // ==> Rota responsavel por fazer a validação de usuario logado ou não logado
 router.post('/validate', userController.validateUser)
 
-
-
-// ==> Rota responsavel por criar os patrimonios: (GET) localhost:3000/api/v1/create
-router.post('/create',  productController.createPatrimonio)
-
-// ==> Rota responsavel por enviar para o frontend os patrimonios  e informações
-router.get('/listing',  productController.getPatrimonioListing)
-router.get('/all',  productController.allPatrimonio)
-router.get('/patrimonio/:id', productController.searchItem)
-
-// ==> Rota responsavel por atualizar os patrimonios: (GET) localhost:3000/api/v1/update/:id
-router.post('/update/:id',  productController.updatePatrimonio)
-
-// ==> Rota responsavel por atualizar os patrimonios: (GET) localhost:3000/api/v1/update/:id
-router.post('/delete/:id',  productController.deletePatrimonio)
-
-// ==> Rota responsavel por procurar os patrimonios: (GET) localhost:3000/api/v1/search/:id
-router.post('/search/:id', productController.searchItem)
-
-router.get('/patrimonios', productController.countPatrimonios)
 
 
 
