71b46af559641cb83cac1a259d4139710f90e110
Fulozz <92411755+Fulozz@users.noreply.github.com>
2023-11-01 10:38:32 -0300

    Update final

--- client/src/API/API.user.js
+++ client/src/API/API.user.js
@@ -3,5 +3,5 @@ const URL = "http://10.0.50.39:3001/api/v1/user"
 const URLocal = "http://localhost:3001/api/v1/user"
 export default () => axios.create({
   // Conexao com o backend
-  baseURL: URLocal,
+  baseURL: URL,
 });
