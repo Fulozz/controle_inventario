85430d63a48492aadb1e168c7f25f8fa1638d825
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2023-12-04 17:36:50 -0300

    AAAAA

--- server/src/routes/Patrimonio.routes.js
+++ server/src/routes/Patrimonio.routes.js
@@ -11,7 +11,7 @@ const productController = require('../controller/patrimonio.controller')
 
 
 
-// ==> Rota responsavel por criar os patrimonios: (GET) localhost:3000/api/v1/create
+// ==> Rota responsavel por criar os patrimonios: (GET) localhost:3001/api/v1/create
 router.post('/create',  productController.createPatrimonio)
 
 // ==> Rota responsavel por enviar para o frontend os patrimonios  e informações
@@ -19,13 +19,13 @@ router.get('/listing',  productController.getPatrimonioListing)
 router.get('/all',  productController.allPatrimonio)
 router.get('/patrimonio', productController.searchItem)
 
-// ==> Rota responsavel por atualizar os patrimonios: (GET) localhost:3000/api/v1/update/:id
+// ==> Rota responsavel por atualizar os patrimonios: (GET) localhost:3001/api/v1/update/:id
 router.post('/update/:id',  productController.updatePatrimonio)
 
-// ==> Rota responsavel por atualizar os patrimonios: (GET) localhost:3000/api/v1/update/:id
+// ==> Rota responsavel por atualizar os patrimonios: (GET) localhost:3001/api/v1/update/:id
 router.delete('/delete/:id',  productController.deletePatrimonio)
 
-// ==> Rota responsavel por procurar os patrimonios: (GET) localhost:3000/api/v1/search/:id
+// ==> Rota responsavel por procurar os patrimonios: (GET) localhost:3001/api/v1/search/:id
 router.post('/search/:id', productController.searchItem)
 router.post('/search/serial/:id', productController.searchSerialNumber)
 
--- server/src/routes/User.routes.js
+++ server/src/routes/User.routes.js
@@ -12,18 +12,18 @@ const userController = require('../controller/user.controller');
 
 const auth  = require('../middleware/auth');
 
-// ==> Rota responsavel por criar o novo 'User': (POST) localhost:3000/api/v1/register
+// ==> Rota responsavel por criar o novo 'User': (POST) localhost:3001/api/v1/register
 router.post('/register', userController.registerNewUser);
 
-// ==> Rota responsavel por fazer o login de usuarios e verifica;'ao: (POST) localhost:3000/api/v1/login
+// ==> Rota responsavel por fazer o login de usuarios e verifica;'ao: (POST) localhost:3001/api/v1/login
 router.post('/login', userController.loginUser);
 
 // ==> Rota responsavel por fazer a validação de usuario logado ou não logado
 router.post('/validate', userController.validateUser);
 
-// ==> Rota responsavel por acessar o perfil do usuario: (GET) localhost:3000/api/v1/dashboard
+// ==> Rota responsavel por acessar o perfil do usuario: (GET) localhost:3001/api/v1/dashboard
 router.get('/dashboard', auth, userController.returnUserProfile);
 
-// ==> Rota responsavel por acessar o perfil do usuario: (GET) localhost:3000/api/v1/user
+// ==> Rota responsavel por acessar o perfil do usuario: (GET) localhost:3001/api/v1/user
 router.post('/user', userController.returnUserProfile);
 module.exports = router
